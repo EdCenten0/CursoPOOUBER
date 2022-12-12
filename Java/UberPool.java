031666150efa5226582aebb5bd53381b80de09a3
EdCenten0 <cchavarriacenteno8@gmail.com>
2022-12-11 20:11:24 -0600

    xd

--- Java/UberPool.java
+++ Java/UberPool.java
@@ -17,6 +17,18 @@ public class UberPool extends Car{
         this.brand = brand;
         this.model = model;
     }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
     
 
     
